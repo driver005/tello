b304e857525d88a463eef09f5dca0b2400a05d91
darklord <github@dark-lord.de>
2021-09-03 15:07:30 +0200

    beta

--- monodepth2/tello.py
+++ monodepth2/tello.py
@@ -5,9 +5,18 @@ import matplotlib as mpl
 import numpy as np
 import PIL.Image as pil
 import matplotlib.pyplot as plt
+from torch._C import parse_schema
 import networks
 import torch
 from torchvision import transforms
+import time
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
 
 model_name = "mono_640x192"
 
@@ -35,13 +44,13 @@ tello.connect()
 
 tello.streamon()
 
-#cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture(0)
 
 tello.takeoff()
 tello.send_rc_control(0, 0, 0, 0)
 
 while True:
-    input_image = tello.get_frame_read().frame #cam.read()  ret, 
+    input_image = tello.get_frame_read().frame  # cam.read()  ret,
     input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
     original_width, original_height = (640, 192)  # input_image.shape
     feed_height = loaded_dict_enc['height']
@@ -74,49 +83,100 @@ while True:
     # plt.imshow(disp_resized_np, cmap='magma', vmax=vmax)
     # plt.title("Disparity prediction", fontsize=22)
     # plt.axis('off')
-    
+
     # Bereich zum suchen
     small_fov_out = disp_resized_np[48:96, 160:320]
-    
+
     # Nächste punkt
     max = np.max(small_fov_out)
     position_min = np.where(disp_resized_np == max)
 
-    # Entferntester Bereich
-    step_size = 25
-    size = 50
-    for y in range(0, original_height - size -1, step_size):
-        for x  in range(0, original_width - size - 1, step_size):
-            #image = cv2.rectangle(disp_resized_np, (x, y), (x + size, y + size ), (255, 0, 0), 2)
-            part_img = disp_resized_np[y:y + size, x:x + size]
-            for array in part_img:
-                for num in array:
-                    if num > 0.2:
-                        viabile_img_part = False
-                        break
-                    else:
-                        viabile_img_part = True
-            if viabile_img_part:
-                image = cv2.rectangle(disp_resized_np, (x, y), (x + size, y + size ), (255, 0, 0), 2)
-            
-
-
-    
-
     # Einzeichnen
-    #cv2.putText(disp_resized_np, "min: " + str(min))
+    # cv2.putText(disp_resized_np, "min: " + str(min))
     image = cv2.circle(disp_resized_np, (position_min[1][0]+5, position_min[0][0]+5), radius=10,
                        color=(0, 0, 255), thickness=-1)
     print(max)
-    cv2.imshow("test", disp_resized_np)
-    if(max > 0.5):
-        
-        tello.send_rc_control(0, -20, 0, 0)
-        print("land")
-        tello.land
-        break
+
+    if(max > 0.4):
+
+        step_size = 25
+        size = 50
+        passed_cubes = []
+        for y in range(0, original_height - size - 1, step_size):
+            for x in range(0, original_width - size - 1, step_size):
+                # image = cv2.rectangle(disp_resized_np, (x, y), (x + size, y + size ), (255, 0, 0), 2)
+                part_img = disp_resized_np[y:y + size, x:x + size]
+                for array in part_img:
+                    for num in array:
+                        if num > 0.2:
+                            viabile_img_part = False
+                            break
+                        else:
+                            viabile_img_part = True
+                    if(viabile_img_part == True):
+                        # image = cv2.rectangle(
+                        #     disp_resized_np, (x, y), (x + size, y + size), (255, 0, 0), 2)
+                        passed_cubes.append((x + (size / 2), y + (size / 2)))
+        if(len(passed_cubes) == 0):
+            tello.send_rc_control(0, 0, 30, 0)
+
+            # print("land")
+            # tello.land
+            # break
+        else:
+            middleX = original_width / 2
+            middleY = original_height / 2
+
+            differences = []
+
+            for cube in passed_cubes:
+                total_difference = 0
+                if(cube[0] > middleX and cube[1] > middleY):
+                    total_difference = (
+                        cube[0] - middleX) + (cube[1] + middleY)
+                if(cube[0] < middleX and cube[1] > middleY):
+                    total_difference = (
+                        middleX - cube[0]) + (cube[1] + middleY)
+                if(cube[0] < middleX and cube[1] < middleY):
+                    total_difference = (
+                        middleX - cube[0]) + (middleY + cube[1])
+                if(cube[0] > middleX and cube[1] < middleY):
+                    total_difference = (
+                        cube[0] - middleX) + (middleY + cube[1])
+                differences.append(total_difference)
+
+            min_varieation_idx = np.min(differences)
+
+            # least_variation = find_nearest(passed_cubes, (middleX, middleY))
+            least_variation = passed_cubes[min_varieation_idx]
+
+            image = cv2.rectangle(
+                disp_resized_np,
+                ((int(least_variation[0])) - int((size / 2)),
+                 int(least_variation[1]) - int((size / 2))),
+                ((int(least_variation[0])) + int((size / 2)),
+                 int(least_variation[1]) + int((size / 2))),
+                (255, 0, 0),
+                2
+            )
+
+            print(least_variation)
+
+            tello.send_rc_control(0, 0, 0, 0)
+
+            time.sleep(3)
+
+            tello.land()
+
+            # for()
+        # tello.send_rc_control(0, -20, 0, 0)
+        # print("land")
+        # tello.land
+        # break
     else:
         tello.send_rc_control(0, 30, 0, 0)
+
+    cv2.imshow("test", disp_resized_np)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
