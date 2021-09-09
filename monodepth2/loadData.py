fdb67fa4c4c24f6b9dbaacdc22b8f0681b677b7f
SteffenGT <stefengthomas@gmail.com>
2021-09-09 17:39:30 +0200

    beta

--- monodepth2/loadData.py
+++ monodepth2/loadData.py
@@ -1,6 +1,7 @@
 import  numpy as np
 import  cv2
 from os import listdir
+from keras.preprocessing.image import ImageDataGenerator
 
 def load_images(wide, height, path):
 
@@ -18,8 +19,8 @@ def load_images(wide, height, path):
     return array
 
 
-X_positive = load_images(1, 1, "C://Users/steffen/LuftbildarchiologiePositiv/")
-X_negative = load_images(1, 1, "C://Users/steffen/LuftbildarchiologieNegativ/")
+X_positive = load_images(128, 128, "C://Users/steffen/LuftbildarchiologiePositiv/")
+X_negative = load_images(128, 128, "C://Users/steffen/LuftbildarchiologieNegativ/")
 print(X_positive.shape)
 print(X_negative.shape)
 All_X = np.concatenate((X_positive, X_negative), axis=0)
@@ -32,3 +33,21 @@ print(Y_train_all)
 
 print(All_X.shape)
 print(All_X)
+
+gen = ImageDataGenerator(width_shift_range=3,
+                         height_shift_range=3,
+                         zoom_range=0.1,
+                         horizontal_flip=True,
+                         vertical_flip=True
+                         )
+"""
+print(gen.flow(All_X, Y_train_all, shuffle=True, batch_size=4583))
+
+print(Y_train_all)
+print(Y_train_all.shape)
+
+print(All_X.shape)
+print(All_X)
+"""
+np.save("C://Users/steffen/repos/tello/monodepth2/X_train_non_gen.npy", All_X, allow_pickle=False)
+np.save("C://Users/steffen/repos/tello/monodepth2/Y_train_non_gen.npy", Y_train_all, allow_pickle=False)
